52306be1b66bbffef8ef1a084b61e5348b27723a
MargheritaCavattoni <62807502+MargheritaCavattoni@users.noreply.github.com>
2022-04-21 14:50:36 +0200

    Update on sigle end and long reads

--- src/ClassGraph.py
+++ src/ClassGraph.py
@@ -1,23 +1,16 @@
-import sys
-import os
-import subprocess
-import csv
-import operator
-import time
 import argparse
-import re
 import logging
 from igraph import *
 from labprop.LabelPropagation import lp1, lp2
-
-
-
 
 # Sample command
 # -------------------------------------------------------------------
 # python ReadGraph_SGA.py     --graph /path/to/graph_file.asqg
 #                            --binned /path/to/binning_result.csv
 #                            --output /path/to/output_folder
+#                            --max_iteration maximum number of iterations
+#                            --read_type 1 paired-end 2 single-end
+#                            --assembler 1 sga 2 minimap2
 # -------------------------------------------------------------------
 
 
@@ -30,8 +23,6 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 consoleHeader = logging.StreamHandler()
 consoleHeader.setFormatter(formatter)
 logger.addHandler(consoleHeader)
-
-start_time = time.time()
 
 # Setup argument parser
 # -----------------------
@@ -47,6 +38,11 @@ ap.add_argument("--max_iteration", required=False, type=int, default=20,
                 help="maximum number of iterations for label propagation algorithm. [default: 20]")
 ap.add_argument("--lp_version", required=False, type=int, default=1,
                 help="Type 1 if you want to propagate with lp-v1, type 2 if you prefer to use lp-v2. [default 1]")
+ap.add_argument("--read_type", required=False, type=int, default=1,
+                help="Type 1 if the reads are paired, type 2 if reads are single-end[default 1]")
+ap.add_argument("--assembler", required=False, type=int, default=1,
+                help="Type 1 if the assembler is sga, type 2 if is minimap2")
+
 args = vars(ap.parse_args())
 
 sgafile = args["graph"]
@@ -56,7 +52,9 @@ kraken2_file = args["binned"]
 output_path = args["output"]
 prefix = args["prefix"]
 max_iteration = args["max_iteration"]
-labprop_v= args["lp_version"]
+labprop_v = args["lp_version"]
+read_type = args["read_type"]
+assembler = args["assembler"]
 
 # Setup output path for log file
 # ---------------------------------------------------
@@ -66,9 +64,7 @@ fileHandler.setLevel(logging.INFO)
 fileHandler.setFormatter(formatter)
 logger.addHandler(fileHandler)
 
-logger.info(
-    "ReadGraph makes use of the assembly graph produced by SGA")
-
+logger.info("ReadGraph makes use of the assembly graph produced by the assembler")
 logger.info("Overlap graph file: " + sgafile)
 logger.info("Existing binning output file: " + kraken2_file)
 logger.info("Final binning output file: " + output_path)
@@ -82,123 +78,116 @@ logger.info("ReadGraph started")
 logger.info("Combining the graph file with the classification one, in order to obtain a labelled graph")
 
 krakenlabels = []
-edges = []
-
-with open(sgafile, 'r') as infile1, open(kraken2_file, 'r') as infile2:
-    for line2 in infile2:
-        krakenlabel = []
-        krakenlabel.append(line2.split()[0])
-        krakenlabel.append(line2.split()[1])
-        # if line2.split()[1] == "1":
-        #     print(line2.split()[0])
-        krakenlabels.append(krakenlabel)
-    for line in infile1:
-        if line.startswith("VT"):
-            words = line.split()
-            readslen = len(words[2])
-            break
-    # print(readslen)
-    prevsuffix = 0
-    for line in infile1:
-        if line.startswith("ED"):
-            words = line.split()
-            firststring = words[1]
-            secondstring = words[2]
-            firststringlen = len(firststring)
-            secondstringlen = len(secondstring)
-            newfirststr = firststring[:firststringlen-2]
-            newsecondstr = secondstring[:secondstringlen - 2]
-            overlapstart = int(words[3])
-            overlapend = int(words[4])
-            normalizedoverlaplength = float((overlapend - overlapstart + 1)/readslen)
-            tmpsuffix = newfirststr.split(".", 1)[1]
-            if tmpsuffix != prevsuffix:
-                prevsuffix = tmpsuffix
-                edge = []
-                edge.append(newfirststr)
-                edge.append(newsecondstr)
-                edge.append(normalizedoverlaplength)
-                edges.append(edge)
-                subgroupcnt = 1
-            else:
-                alreadypresent = False
-                for k in range(len(edges)-1-subgroupcnt, len(edges)):
-                    if edges[k][1] == newsecondstr:
-                        edges[k][2] = max(edges[k][2], normalizedoverlaplength)
-                        alreadypresent = True
-                        break
-                if alreadypresent == False:
-                    edge = []
-                    edge.append(newfirststr)
-                    edge.append(newsecondstr)
-                    edge.append(normalizedoverlaplength)
-                    edges.append(edge)
-                    subgroupcnt += 1
-
-
-# Get the links from the .asqg file
-# -----------------------------------
-
 links = []
 
-my_map = []
+read_dict = {}
+inv_read_dict = {}
 
-node_count = 0
-
-binned_reads = []
-
-#binned_reads_info = []
 reads_info = []
 
-read_groups = []
-
-try:
-    common = ""
-    commonend = "."
-
-    for i in range(len(krakenlabels)):
-        actual_common = krakenlabels[i][0].split(".", 1)[0]
-        if common != actual_common:
-            read_group = []
-            read_group.append(actual_common)
-            read_group.append(node_count)
-            read_groups.append(read_group)
-            common = actual_common
-
-        if krakenlabels[i][1] != "0":
-            binned_reads.append(node_count)
-
-        reads_info.append(krakenlabels[i][1])
-        read_id = krakenlabels[i][0]
-        my_map.append(read_id)
-        node_count += 1
-
-    # for i in range(len(read_groups)):
-    #     print(read_groups[i])
+links_dict = {}
+index = 0
+bool_idx1 = False
+bool_idx2 = False
+max_seq_len = 0
 
 
-    for j in range(len(edges)):
-        link = []
-        common1 = edges[j][0].split(".", 1)[0]
-        for i in range(len(read_groups)):
-            if common1 == read_groups[i][0]:
-                link.append(int(read_groups[i][1] + int(edges[j][0][(len(common1) + 1):]) - 1))
-                break
-        common2 = edges[j][1].split(".", 1)[0]
-        for i in range(len(read_groups)):
-            if common2 == read_groups[i][0]:
-                link.append(int(read_groups[i][1] + int(edges[j][1][(len(common2) + 1):]) - 1))
-                break
-        link.append(float(edges[j][2]))
-        links.append(list(link))
+def add_to_dicts(read_id):
+    read_dict[index] = read_id
+    inv_read_dict[read_id] = index
 
-# for i in range(len(links)):
-#     print(links[i])
 
-except:
-    logger.error("Please make sure that the correct path to the assembly graph file is provided.")
-    logger.info("Exiting ReadGraph!")
-    sys.exit(1)
+def compute_normalize_overlap_len(assembler, line, max_seq_len):
+    if assembler == 1:
+        overlapstart = int(line.split()[3])
+        overlapend = int(line.split()[4])
+        normalizedoverlaplength = float((overlapend - overlapstart + 1) / max_seq_len)
+    else:
+        overlaplen = int(line.split()[11])
+        normalizedoverlaplength = float(overlaplen / max_seq_len)
+    return normalizedoverlaplength
+
+
+with open(sgafile, 'r') as graph_file, open(kraken2_file, 'r') as classification_file:
+    # Build the following dictionary: {0: 'read_id1', 1: 'read_id2, ...}
+    for line in classification_file:
+        read_id = line.split()[0]
+        tax_id = line.split()[1]
+        read_dict[index] = read_id
+        reads_info.append(tax_id)
+        index += 1
+
+    index -= 1
+
+    inv_read_dict = {v: k for k, v in read_dict.items()}
+
+    for line in graph_file:
+        if line.startswith("ED"):
+            seq1_len = int(line.split()[5])
+            seq2_len = int(line.split()[8])
+            max_seq_len = max(max_seq_len, seq1_len, seq2_len)
+
+    graph_file.seek(0)
+
+    for line in graph_file:
+        if line.startswith("VT"):
+            # If we have paired end read
+            if read_type == 1:
+                read_id = line.split()[1][:-2]
+                if read_id not in inv_read_dict:
+                    reads_info.append(0)
+                    index += 1
+                    add_to_dicts(read_id)
+            else:
+                read_id = line.split()[1]
+                if read_id not in inv_read_dict:
+                    reads_info.append(0)
+                    index += 1
+                    add_to_dicts(read_id)
+        if line.startswith("ED"):
+            if read_type == 1:
+                read1_id = line.split()[1][:-2]
+                read2_id = line.split()[2][:-2]
+            else:
+                read1_id = line.split()[1]
+                read2_id = line.split()[2]
+            if read1_id not in inv_read_dict:
+                reads_info.append(0)
+                index += 1
+                index_r1 = index
+                bool_idx1 = True
+                add_to_dicts(read1_id)
+            if read2_id not in inv_read_dict:
+                reads_info.append(0)
+                index += 1
+                index_r2 = index
+                bool_idx2 = True
+                add_to_dicts(read2_id)
+            if bool_idx1 == True:
+                bool_idx1 = False
+                in_dict_1 = int(index_r1)
+            else:
+                in_dict_1 = int(inv_read_dict[read1_id])
+            if bool_idx2 == True:
+                bool_idx2 = False
+                in_dict_2 = int(index_r2)
+            else:
+                in_dict_2 = int(inv_read_dict[read2_id])
+            normalizedoverlaplength = compute_normalize_overlap_len(assembler, line, max_seq_len)
+            to_dict_key = str(in_dict_1) + " " + str(in_dict_2)
+            if assembler == 1:
+                if to_dict_key in links_dict:
+                    prev_val = links_dict[to_dict_key]
+                    new_val = max(prev_val, normalizedoverlaplength)
+                    links_dict[to_dict_key] = new_val
+                else:
+                    links_dict[to_dict_key] = normalizedoverlaplength
+            else:
+                links_dict[to_dict_key] = normalizedoverlaplength
+
+for key, value in links_dict.items():
+    tmp = [int(key.split()[0]), int(key.split()[1]), value]
+    links.append(tmp)
 
 # Construct the assembly graph
 # -------------------------------
@@ -214,19 +203,18 @@ try:
     weights_list = []
 
     # Add vertices
-    reads_graph.add_vertices(node_count)
+    reads_graph.add_vertices(index + 1)
 
     # Name vertices
     for i in range(len(reads_graph.vs)):
         reads_graph.vs[i]["id"] = i
-        reads_graph.vs[i]["label"] = str(my_map[i])
+
     # Iterate links
     for i in range(len(links)):
         # Remove self loops
         if links[i][0] != links[i][1]:
             edge_list.append((int(links[i][0]), int(links[i][1])))
             weights_list.append((float(links[i][2])))
-
 
     reads_graph.add_edges(edge_list)
 
@@ -239,10 +227,6 @@ except:
 
 logger.info("Total number of edges in the assembly graph: " + str(len(edge_list)))
 
-
-# Run label propagation
-# -----------------------
-
 logger.info("Preparing data for label propagation")
 
 # In the graph are not inserted edges that connect already labbeled nodes
@@ -250,14 +234,10 @@ logger.info("Preparing data for label propagation")
 data = []
 LabbeledVertices = []
 
-for read in range(node_count):
-
-    line = []
-    line.append(read)
+for read in range(index + 1):
+    line = [read]
     if int(reads_info[read]) != 0:
-        alreadyLabelled = []
-        alreadyLabelled.append(read)
-        alreadyLabelled.append(int(reads_info[read]))
+        alreadyLabelled = [read, int(reads_info[read])]
         LabbeledVertices.append(alreadyLabelled)
 
     line.append(int(reads_info[read]))
@@ -266,28 +246,23 @@ for read in range(node_count):
 
     for neighbour in neighbours:
         if int(reads_info[neighbour]) == 0:
-            n = []
-            n.append(neighbour)
+            n = [neighbour]
             e_weight = reads_graph.es[reads_graph.get_eid(read, neighbour)]["weight"]
             n.append(e_weight)
             neighs.append(n)
-
 
     line.append(neighs)
     if labprop_v == 2:
         line.append(0)
     data.append(line)
 
-
-logger.info("Starting label propagation")
-
-# The propagation at each iteration is performed from the last labelled nodes to their neighbors
-# Once a node is labbeled , that label is not going to be changed
 try:
 
     if labprop_v == 1:
+        logger.info("Starting label propagation, version V1")
         lp1(max_iteration, data)
     else:
+        logger.info("Starting label propagation, version V2")
         lp2(max_iteration, data)
 
 except:
@@ -295,27 +270,11 @@ except:
     logger.info("Exiting ClassGraph.. Bye...!")
     sys.exit(1)
 
-
-
-logger.info("***************Label propagation termined**************")
-
-output_file = output_path + prefix + '.res'
-
-# with open(output_file, mode='w') as out_file:
-#     for i in range(len(data)):
-#         read_seqid = data[i][0]
-#         rg_len = len(read_groups)
-#         if rg_len == 1:
-#             read_id = read_groups[0][0] + "." + str(read_seqid + 1)
-#         elif rg_len > 1:
-#             for k in range(rg_len):
-#                 if read_seqid >= read_groups[rg_len-k-1][1]:
-#                     read_id = read_groups[rg_len-k-1][0] + "." + str(read_seqid - read_groups[rg_len-k-1][1] + 1)
-#                     break
-#         out_file.write(read_id + "\t" + str(data[i][1]) + "\n")
+output_file = output_path + prefix + 'CG.res'
 
 with open(output_file, mode='w') as out_file:
     for i in range(len(data)):
-        out_file.write(krakenlabels[i][0] + "\t" + str(data[i][1]) + "\n")
+        out_file.write(read_dict[data[i][0]] + "\t" + str(data[i][1]) + "\n")
 
-logger.info("Final binning results can be found at " + output_file)
+logger.info("You will find the output here: " + output_file )
+logger.info("***************Label propagation termined**************")
